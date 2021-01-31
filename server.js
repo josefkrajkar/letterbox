723476a63728083477d50e90b0c3cb763e5fd460
josefkrajkar <josef.krajkar@mensa.cz>
2021-01-31 18:00:23 +0100

    remove front-end to different deployment

--- server.js
+++ server.js
@@ -1,19 +1,54 @@
 const express = require('express');
-const app = express();
-const { createServer } = require('http');
-const server = createServer(express);
+const cors = require('cors');
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3000;
-}
-const bodyParser = require('body-parser');
+  port = 3001;
+};
 const rooms = [];
-const {parse} = require('url');
 const clients = [];
 const UUID = require('uuid');
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ server })
-const wsPort = 6969;
+const bodyParser = require('body-parser');
+const {parse} = require('url');
+
+const server = express()
+
+  .use(bodyParser.urlencoded({extended: false}))
+  .use(bodyParser.json())
+  .use(cors())
+
+  .post('/api/createRoom', (req, res) => {
+    const uuid = UUID.v4();
+    rooms.push({
+      id: uuid,
+      name: req.body.name,
+      pass: req.body.pass
+    });
+    res.status(200);
+    res.json({uuid});
+  })
+
+  .post('/api/enterRoom', (req, res) => {
+    const roomIndex = rooms.findIndex(item => item.name === req.body.name);
+    if (roomIndex > -1 && req.body.pass === rooms[roomIndex].pass) {
+      res.status(200);
+      res.json({uuid: rooms[roomIndex].id});
+    } else {
+      res.status(404);
+      res.send('KO');
+    }
+  })
+
+  .get('/ping', (req, res) => {
+    res.status(200);
+    res.send('pong');
+  })
+
+  .listen(port, () => {
+    console.log(`> App listening at http://localhost:${port}`);
+  });
+
+const { Server } = require('ws');
+const wss = new Server({ server });
 
 wss.on('connection', function connection(ws, req) {
 
@@ -37,38 +72,4 @@ wss.on('connection', function connection(ws, req) {
       });
     }
   });
-});
-
-server.listen(wsPort, function() {
-  console.log(`> Websocket ready on ws://localhost:${wsPort}`)
-});
-
-app.use(express.static("front-end"));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.post('/api/createRoom', (req, res) => {
-  const uuid = UUID.v4();
-  rooms.push({
-    id: uuid,
-    name: req.body.name,
-    pass: req.body.pass
-  });
-  res.status(200);
-  res.json({uuid});
-});
-
-app.post('/api/enterRoom', (req, res) => {
-  const roomIndex = rooms.findIndex(item => item.name === req.body.name);
-  if (roomIndex > -1 && req.body.pass === rooms[roomIndex].pass) {
-    res.status(200);
-    res.json({uuid: rooms[roomIndex].id});
-  } else {
-    res.status(404);
-    res.send('KO');
-  }
-});
-
-app.listen(port, () => {
-  console.log(`> App listening at http://localhost:${port}`);
 });
